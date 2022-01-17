83b3272564a7b366d3962530a1ef6e86a7b6af77
stereaam <mihai.sterea@yahoo.com>
2022-01-17 13:44:41 +0200

    created main pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import Favorites from './Pages/Favorites'
 
 function App() {
   return (
     <div className="App">
- 
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/favorites' component={Favorites}/>
+      </Switch>
     </div>
   );
 }
