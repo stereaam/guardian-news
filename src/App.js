4ff285a831be2fb4f5da89360f2856c55f1c18df
stereaam <mihai.sterea@yahoo.com>
2022-01-19 15:10:14 +0200

    create route for article page and 404 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom'
 import Home from './Pages/Home'
 import Favorites from './Pages/Favorites'
+import Page404 from './Pages/Page404'
+import Article from './Pages/Article'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route path='/favorites' component={Favorites}/>
+        <Route path='/article/:articleId+' component={Article}/>
+        <Route path='*' component={Page404}/>
       </Switch>
     </div>
   );
