ec56fbe02f8327d9bd8d273736338505689f6413
stereaam <mihai.sterea@yahoo.com>
2022-01-19 15:10:40 +0200

    created page 404

---
+++ src/Pages/Page404.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../Components/Layout'
+
+function Page404() {
+    return (
+        <div>
+            <Layout>
+                <div className='h1 d-flex align-items-center justify-content-center'>
+                    404 NOT FOUND
+                </div>
+            </Layout>
+        </div>
+    )
+}
+
+export default Page404
