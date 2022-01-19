69ba6f291af834225032f2ef43067406ec012ad0
stereaam <mihai.sterea@yahoo.com>
2022-01-19 15:10:58 +0200

    update home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,53 +2,105 @@ import React from 'react'
 import Layout from '../Components/Layout'
 import {connect} from 'react-redux'
 import {getNews} from '../redux/news/newsActions'
+import {addToFavorites} from '../redux/favorites/favoritesActions'
 import {Link} from 'react-router-dom'
+import './Home.css'
 
 
 
 class Home extends React.Component {
 
-        constructor(){
-            super()
-            this.state = {
-                newsList: []
-            }
-        }
         componentDidMount(){
-            this.props.getNews()
+            const queryString = this.props.location.search;
+            const pageNumber = this.getPageNumberFromQueryString(queryString);
+            this.props.getNews({
+                page: pageNumber
+            })
         }
 
         componentDidUpdate(prevProps){
-            if(prevProps.newsList!== this.props.newsList){
-                this.setState({newsList: this.props.newsList})        
+            const queryString = this.props.location.search;
+            const pageNumber = this.getPageNumberFromQueryString(queryString);
+            if(prevProps.location.search !== queryString){
+                this.props.getNews({
+                    page: pageNumber
+                })     
             }
         }
 
+        getPageNumberFromQueryString(queryString) {
+            return queryString ? Number(queryString[queryString.length - 1]) : 1
+        }
+
+        goToPage(page) {
+            const {history} = this.props
+            const pageQueryString = page === 1 ? '' : `?page=${page}`;
+            history.push({search: pageQueryString});
+        }
+
         render(){
+            const queryString = this.props.location.search
+            const {addToFavorites, newsList} = this.props
             return (  
                 <div>
                     <Layout>
-                        <div className="row">                         
-                            {this.state.newsList.map((news) => {
+                        <div className="row mt-1">                     
+                            {newsList.map((news) => {
                                 return (
-                                    <div className="article-box col-12 col-lg-6 mb-4 pl-2 pr-5 py-3" key={news.id}>
-                                    <Link to={`/article/${news.id}`}>
-                                        <h1 className="text-title h3">{news.webTitle}</h1>
-                                        <p className="text-subtitle h5">{news.pillarName} {'>'} {news.sectionName}</p>
-                                    </Link>
-                                
-                                
-                                        <div className="d-flex justify-content-end">
-                                            <button
-                                                className="btn btn-outline-dark"
-                                            >
-                                                Add to favorites
-                                            </button>
+                                    <div className="col-12 col-lg-6" key={news.id}>
+                                        <div className='home-article d-flex flex-column justify-content-between rounded my-3 px-3 py-1'>
+                                            <Link className='text-decoration-none text-black' to={`/article/${news.id}`}>
+                                                <h1 className="h3">{news.webTitle}</h1>
+                                                <p className="h5">
+                                                    {news.pillarName} {'>'} {news.sectionName}
+                                                </p>
+                                            </Link>
+                                        
+                                            <div className="d-flex justify-content-end align-items-end">
+                                                <button 
+                                                    className="btn btn-danger m-0"
+                                                    onClick={()=> addToFavorites({
+                                                        id: news.id,
+                                                        title: news.webTitle,
+                                                        pillarName: news.pillarName,
+                                                        sectionName: news.sectionName
+                                                    })}>
+                                                    Add to favorites
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                 )})
                             }
                         </div>
+                        <nav className="d-flex justify-content-center my-3"> 
+                            <button 
+                                className={
+                                    this.getPageNumberFromQueryString(queryString) === 1
+                                        ? 'btn btn-dark m-1' 
+                                        : 'btn btn-outline-dark m-1'} 
+                                onClick={() => this.goToPage(1)}
+                            >
+                                1
+                            </button>
+                        
+                            <button 
+                            className={
+                                    this.getPageNumberFromQueryString(queryString) === 2
+                                        ? 'btn btn-dark m-1' 
+                                        : 'btn btn-outline-dark m-1'}
+                            onClick={() => this.goToPage(2)}>
+                                2
+                            </button>
+                            <button 
+                            className={
+                                    this.getPageNumberFromQueryString(queryString) === 3
+                                        ? 'btn btn-dark m-1' 
+                                        : 'btn btn-outline-dark m-1'}
+                            onClick={() => this.goToPage(3)}>
+                                3
+                            </button> 
+                        </nav>
                     </Layout>
                 </div>
             )
@@ -57,12 +109,14 @@ class Home extends React.Component {
 
 function mapStateToProps(state){
     return{
-        newsList: state.data
+        newsList: state.news.data
     }
 }
+
 function mapDispatchToProps(dispatch){
     return {
-        getNews: () => dispatch(getNews())
+        getNews: (payload) => dispatch(getNews(payload)),
+        addToFavorites: (payload) => dispatch(addToFavorites(payload))
     }
 }
 
